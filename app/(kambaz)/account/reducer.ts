68044affa67ef3b2bb25a75cb0cfb9e2703b9066
LondonJones <londonjonez00@gmail.com>
2026-04-12 19:03:39 -0400

    changes

--- app/(kambaz)/account/reducer.ts
+++ app/(kambaz)/account/reducer.ts
@@ -1,20 +1,32 @@
-
+"use client";
 import { createSlice } from "@reduxjs/toolkit";
-import { users } from "../database";
 
 const initialState = {
-  currentUser: users[0],
+ courses: [] as any[],
 };
-
-const accountSlice = createSlice({
-  name: "account",
-  initialState,
-  reducers: {
-    setCurrentUser: (state, action) => {
-      state.currentUser = action.payload;
-    },
-  },
+const coursesSlice = createSlice({
+ name: "courses",
+ initialState,
+ reducers: {
+   addNewCourse: (state, { payload: course }) => {
+     const newCourse = { ...course, _id: course._id };
+     state.courses = [...state.courses, newCourse] as any;
+   },
+   deleteCourse: (state, { payload: courseId }) => {
+     state.courses = state.courses.filter(
+       (course: any) => course._id !== courseId
+     );
+   },
+   updateCourse: (state, { payload: course }) => {
+     state.courses = state.courses.map((c: any) =>
+       c._id === course._id ? course : c
+     ) as any;
+   },
+   setCourses: (state, { payload: courses }) => {
+     state.courses = courses;
+   },
+ },
 });
-
-export const { setCurrentUser } = accountSlice.actions;
-export default accountSlice.reducer;
+export const { addNewCourse, deleteCourse, updateCourse, setCourses } =
+ coursesSlice.actions;
+export default coursesSlice.reducer;
