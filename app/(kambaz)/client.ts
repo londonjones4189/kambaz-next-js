73168422652bcdb9340764a9a9869eab0277c2d8
LondonJones <londonjonez00@gmail.com>
2026-04-12 12:07:46 -0400

    a6 homework

--- app/(kambaz)/client.ts
+++ app/(kambaz)/client.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 const ENROLLMENTS_API = `${HTTP_SERVER}/api/enrollments`;
+
+
  
 export const findAllEnrollments = async () => {
   const { data } = await axiosWithCredentials.get(ENROLLMENTS_API);
--- app/(kambaz)/courses/[cid]/people/table/page.tsx
+++ app/(kambaz)/courses/[cid]/people/table/page.tsx
@@ -1,19 +1,26 @@
 "use client";
-import React from "react";
-import { useParams } from "next/navigation";
-import * as db from "../../../../database";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../../store";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
+import PeopleDetails from "../Deatils";
+import * as client from "../../../../account/client";
+import Link from "next/link";
 
-export default function PeopleTable() {
-  const { cid } = useParams();
-  const { users } = db;
-  const { enrollments } = useSelector((state: RootState) => state.enrollmentReducer);
+export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
 
   return (
     <div id="wd-people-table">
+      {showDetails && (
+        <PeopleDetails
+          uid={showUserId}
+          onClose={() => {
+            setShowDetails(false);
+            fetchUsers();
+          }}
+        />
+      )}
       <Table striped>
         <thead>
           <tr>
@@ -26,28 +33,28 @@ export default function PeopleTable() {
           </tr>
         </thead>
         <tbody>
-          {users
-            .filter((usr: any) =>
-              enrollments.some(
-                (enrollment: any) =>
-                  enrollment.user === usr._id &&
-                  enrollment.course === cid
-              )
-            )
-            .map((user: any) => (
-              <tr key={user._id}>
-                <td className="wd-full-name text-nowrap">
+          {users.map((user: any) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+                <span
+                  className="text-decoration-none"
+                  onClick={() => {
+                    setShowDetails(true);
+                    setShowUserId(user._id);
+                  }}
+                >
                   <FaUserCircle className="me-2 fs-1 text-secondary" />
                   <span className="wd-first-name">{user.firstName}</span>{" "}
                   <span className="wd-last-name">{user.lastName}</span>
-                </td>
-                <td className="wd-login-id">{user.loginId}</td>
-                <td className="wd-section">{user.section}</td>
-                <td className="wd-role">{user.role}</td>
-                <td className="wd-last-activity">{user.lastActivity}</td>
-                <td className="wd-total-activity">{user.totalActivity}</td>
-              </tr>
-            ))}
+                </span>
+              </td>
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
