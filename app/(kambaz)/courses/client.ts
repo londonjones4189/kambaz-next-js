26a9c6e8e43e190bc3a036f8dec1b137d15c7142
LondonJones <londonjonez00@gmail.com>
2026-04-12 21:26:33 -0400

    safari  2

--- app/(kambaz)/courses/client.ts
+++ app/(kambaz)/courses/client.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
-const USERS_API = `${HTTP_SERVER}/api/users`;
-const COURSES_API = `${HTTP_SERVER}/api/courses`;
-const MODULES_API = `${HTTP_SERVER}/api/modules`;
-const ENROLLMENTS_API = `${HTTP_SERVER}/api/enrollments`;
+const USERS_API = `/api/users`;
+const COURSES_API = `/api/courses`;
+const MODULES_API = `/api/modules`;
+const ENROLLMENTS_API = `/api/enrollments`;
 
 
  
