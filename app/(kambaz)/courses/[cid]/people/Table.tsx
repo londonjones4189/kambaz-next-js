35ae5fe2b99e1198cd3c8f9a67e2aac45948e8be
LondonJones <londonjonez00@gmail.com>
2026-04-12 20:17:39 -0400

    people

--- app/(kambaz)/courses/[cid]/people/Table.tsx
+++ app/(kambaz)/courses/[cid]/people/Table.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React, { useState } from "react";
-import { Table } from "react-bootstrap";
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import PeopleDetails from "./Deatils";
-import * as client from "../../../account/client";
-import Link from "next/link";
+import PeopleDetails from "./Details";
+
+
 
 export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
   const [showDetails, setShowDetails] = useState(false);
@@ -12,16 +11,17 @@ export default function PeopleTable({ users = [], fetchUsers }: { users?: any[];
 
   return (
     <div id="wd-people-table">
-      {showDetails && (
+    {showDetails && (
         <PeopleDetails
           uid={showUserId}
           onClose={() => {
             setShowDetails(false);
             fetchUsers();
-          }}
-        />
+          }} />
       )}
-      <Table striped>
+
+
+      <table className="table table-striped">
         <thead>
           <tr>
             <th>Name</th>
@@ -32,22 +32,24 @@ export default function PeopleTable({ users = [], fetchUsers }: { users?: any[];
             <th>Total Activity</th>
           </tr>
         </thead>
+
+
         <tbody>
-          {users.map((user: any) => (
-            <tr key={user._id}>
+          {users.filter((user: any) => user !== null).map((user: any) => (
+               <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
-                <span
-                  className="text-decoration-none"
+                <span className="text-decoration-none"
                   onClick={() => {
                     setShowDetails(true);
                     setShowUserId(user._id);
-                  }}
-                >
+                  }}>
+
                   <FaUserCircle className="me-2 fs-1 text-secondary" />
                   <span className="wd-first-name">{user.firstName}</span>{" "}
                   <span className="wd-last-name">{user.lastName}</span>
                 </span>
               </td>
+
               <td className="wd-login-id">{user.loginId}</td>
               <td className="wd-section">{user.section}</td>
               <td className="wd-role">{user.role}</td>
@@ -55,8 +57,9 @@ export default function PeopleTable({ users = [], fetchUsers }: { users?: any[];
               <td className="wd-total-activity">{user.totalActivity}</td>
             </tr>
           ))}
+
         </tbody>
-      </Table>
+      </table>
     </div>
   );
 }
---
+++ app/(kambaz)/courses/[cid]/people/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import PeopleTable from "./Table";
+import * as client from "../../client";
+
+export default function People() {
+  const { cid } = useParams();
+  const [users, setUsers] = useState<any[]>([]);
+
+  const fetchUsers = async () => {
+    const users = await client.findUsersForCourse(cid as string);
+    setUsers(users);
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, [cid]);
+
+  return (
+    <div>
+      <h3>People</h3>
+      <PeopleTable users={users} fetchUsers={fetchUsers} />
+    </div>
+  );
+
+  
+}
